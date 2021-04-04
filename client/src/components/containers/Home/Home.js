d9f8f78c19f550ebda7e49d0624e7798f07c2d97
Itay Tur <unlessmusic1@gmail.com>
2021-04-04 14:54:27 +0300

    content

---
+++ client/src/components/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import Content from '../../presentationals/Content/Content';
+
+import classes from './Home.module.css';
+
+const Home = () => {
+    return (
+        <div className={classes.Home}>
+            <Content />
+            form
+        </div>
+    );
+};
+
+export default Home;
